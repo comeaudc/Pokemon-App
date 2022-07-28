2f439c91396e17cbe1afe75f6d23817d3c79f915
Dylan Comeau <comeaudc@comcast.net>
2022-07-28 15:01:11 -0400

    I can view a list of all my pokemon in the browser

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get('/', (req, res) => {
 })
 
 app.get('/pokemon', (req, res) => {
-    res.render('Index')
+    res.render('Index', {pokemon: pokemon})
 });
--- views/Index.jsx
+++ views/Index.jsx
@@ -1,10 +1,10 @@
 const React = require('react');
 
-const headingStyle = {
-    color: "#FFDE00",
-    textAlign: "center",
-    textDecoration: "underline"
-}
+// const headingStyle = {
+//     color: "#FFDE00",
+//     textAlign: "center",
+//     textDecoration: "underline"
+// }
 
 const body = {
     backgroundColor: "#e4000f ",
@@ -13,20 +13,42 @@ const body = {
     right: 0,
     bottom: 0,
     height: "100%",
-    width: "100%"
+    width: "100%",
+    color: "#FFDE00",
+    textDecoration: "underline"
+}
+
+const main = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
 }
 
 
 
 class Index extends React.Component {
     render(){
+        const {pokemon} = this.props;
         return(
             <html style={body}>
             <head>
                 <title>Pokemon</title>
             </head>
-            <body>
-                <h1 style={headingStyle}>See All The Pokemon!</h1>
+            <body style={main}>
+                <h1>See All The Pokemon!</h1><br/>
+                <ul>
+                    {pokemon.map((mons, i) => {
+                        let name = mons.name.split('');
+                        name[0] = name[0].toUpperCase();
+                        name.join('');
+                        return (
+                            <li>
+                                <a href={`/pokemon/${i}`}>{name}</a>
+                                  <br/>
+                            </li>
+                        );
+                    })}
+                </ul>
             </body>
             </html>
         )
